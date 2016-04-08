e8719fc9cecd65dcd96b289ef3801988154e4ae9
Alexandre Arsenault <alx.arsenault@gmail.com>
2016-04-08 12:38:17 -0400

    Fix color attribute.

--- AudioTools/atMenuColorAttribute.cpp
+++ AudioTools/atMenuColorAttribute.cpp
@@ -1,11 +1,3 @@
-//
-//  atMenuColorAttribute.cpp
-//  AudioTools
-//
-//  Created by Alexandre Arsenault on 2016-04-06.
-//  Copyright © 2016 Alexandre Arsenault. All rights reserved.
-//
-
 #include "atMenuAttribute.h"
 #include "atMenuColorAttribute.hpp"
 #include <OpenAX/Button.h>
@@ -41,34 +33,6 @@ namespace inspector {
 
 		ax::Point pos(0, 0);
 		win->node.Add(ax::shared<ax::Label>(ax::Rect(pos, ax::Size(90, 25)), labelInfo, _name));
-
-		//		ax::TextBox::Info txtInfo;
-		//		txtInfo.normal = ax::Color(1.0);
-		//		txtInfo.hover = ax::Color(1.0);
-		//		txtInfo.selected = ax::Color(1.0);
-		//		txtInfo.highlight = ax::Color(0.4f, 0.4f, 0.6f, 0.2f);
-		//		txtInfo.contour = ax::Color(0.88);
-		//		txtInfo.cursor = ax::Color(1.0f, 0.0f, 0.0f);
-		//		txtInfo.selected_shadow = ax::Color(0.8f, 0.8f, 0.8f);
-		//		txtInfo.font_color = ax::Color(0.0);
-		//
-		//		ax::TextBox::Events txt_evts;
-		//
-		//		txt_evts.enter_click = ax::Event::Function([&](ax::Event::Msg* msg) {
-		//			ax::TextBox::Msg* tmsg = static_cast<ax::TextBox::Msg*>(msg);
-		//			std::string msg_str = tmsg->GetMsg();
-		//
-		//			win->PushEvent(Events::ASSIGN_VALUE,
-		//				new ax::Event::SimpleMsg<ax::StringPair>(ax::StringPair(_name, msg_str)));
-		//		});
-		//
-		//		win->node.Add(ax::shared<ax::TextBox>(
-		//			ax::Rect(ax::Point(90, 0), ax::Size(rect.size.x - 90, 25)), txt_evts, txtInfo, "",
-		//_value));
-
-		//		win->node.Add(ax::shared<ax::Button>(
-		//			ax::Rect(ax::Point(90, 0), ax::Size(rect.size.x - 90, 25)), ax::Button::Events(),
-		// ax::Button::Info(), "", ""));
 	}
 
 	void ColorAttribute::OnColorSelect(const ax::ColorPicker::Msg& msg)
@@ -76,9 +40,12 @@ namespace inspector {
 		win->PushEvent(Events::ASSIGN_VALUE,
 			new ax::Event::SimpleMsg<ax::StringPair>(ax::StringPair(_name, msg.GetMsg().ToString())));
 		
+		_color = msg.GetMsg();
+		win->Update();
+		
 		ax::App& app(ax::App::GetInstance());
 		auto w = app.GetPopupManager()->GetWindowTree()->GetTopLevel();
-//		app.GetWindowManager()->RemoveGlobalClickListener(win);
+		//		app.GetWindowManager()->RemoveGlobalClickListener(win);
 		app.GetPopupManager()->SetPastWindow(nullptr);
 		app.GetPopupManager()->UnGrabKey();
 		app.GetPopupManager()->UnGrabMouse();
@@ -95,21 +62,18 @@ namespace inspector {
 
 	void ColorAttribute::OnColorCancel(const ax::ColorPicker::Msg& msg)
 	{
-		win->PushEvent(Events::ASSIGN_VALUE,
-					   new ax::Event::SimpleMsg<ax::StringPair>(ax::StringPair(_name, msg.GetMsg().ToString())));
-		
 		ax::App& app(ax::App::GetInstance());
 		auto w = app.GetPopupManager()->GetWindowTree()->GetTopLevel();
 		//		app.GetWindowManager()->RemoveGlobalClickListener(win);
 		app.GetPopupManager()->SetPastWindow(nullptr);
 		app.GetPopupManager()->UnGrabKey();
 		app.GetPopupManager()->UnGrabMouse();
-		
+
 		w->event.UnGrabKey();
 		w->event.UnGrabMouse();
-		
+
 		w->backbone = nullptr;
-		
+
 		app.GetPopupManager()->GetWindowTree()->GetNodeVector().clear();
 		app.GetPopupManager()->SetPastWindow(nullptr);
 		app.UpdateAll();
@@ -125,16 +89,16 @@ namespace inspector {
 		sld_info.bgColorNormal = ax::Color(0.97);
 		sld_info.bgColorHover = sld_info.bgColorNormal;
 		sld_info.bgColorClicked = sld_info.bgColorNormal;
-		
+
 		sld_info.sliderColorNormal = ax::Color(0.801);
 		sld_info.sliderColorHover = sld_info.sliderColorNormal;
 		sld_info.sliderColorClicked = sld_info.sliderColorNormal;
 		sld_info.sliderContourColor = ax::Color(0.901);
-		
+
 		sld_info.contourColor = ax::Color(0.88);
 		sld_info.backSliderColor = ax::Color(0.0);
 		sld_info.backSliderContourColor = ax::Color(0.0);
-		
+
 		ax::TextBox::Info txtInfo;
 		txtInfo.normal = ax::Color(0.97);
 		txtInfo.hover = txtInfo.normal;
@@ -144,7 +108,7 @@ namespace inspector {
 		txtInfo.cursor = ax::Color(1.0f, 0.0f, 0.0f);
 		txtInfo.selected_shadow = ax::Color(0.8f, 0.8f, 0.8f);
 		txtInfo.font_color = ax::Color(0.0);
-		
+
 		ax::Button::Info btn_info;
 		btn_info.normal = ax::Color(0.97);
 		btn_info.hover = ax::Color(0.99);
@@ -153,16 +117,16 @@ namespace inspector {
 		btn_info.contour = ax::Color(0.88);
 		btn_info.font_color = ax::Color(0.0);
 		btn_info.corner_radius = 0;
-		
+
 		ax::ColorPicker::Info cp_info;
 		cp_info.btn_info = btn_info;
 		cp_info.sld_info = sld_info;
 		cp_info.txt_info = txtInfo;
-	
+
 		ax::ColorPicker::Events cp_evts;
 		cp_evts.select = GetOnColorSelect();
 		cp_evts.cancel = GetOnColorCancel();
-	
+
 		auto c_picker = ax::shared<ax::ColorPicker>(
 			ax::Point(win->dimension.GetAbsoluteRect().position.x - 120, 30), cp_evts, cp_info, _color);
 
@@ -170,24 +134,23 @@ namespace inspector {
 			ax::Window::Ptr(c_picker->GetWindow()));
 
 		c_picker->GetWindow()->backbone = c_picker;
-//		c_picker->GetWindow()->AddConnection(ax::ColorPicker::Events::SELECT, GetOnColorSelect());
+		//		c_picker->GetWindow()->AddConnection(ax::ColorPicker::Events::SELECT, GetOnColorSelect());
 	}
 
 	void ColorAttribute::OnPaint(ax::GC gc)
 	{
 		const ax::Rect rect(win->dimension.GetDrawingRect());
-		
+
 		const ax::Rect color_rect(rect);
-		
-		
+
 		// Draw color rectangle background.
 		int line_index = 0;
-		for(int y = color_rect.position.y; y < color_rect.position.y + color_rect.size.y; y += 5) {
-			for(int x = color_rect.position.x; x < color_rect.position.x + color_rect.size.x - 5; x += 10) {
+		for (int y = color_rect.position.y; y < color_rect.position.y + color_rect.size.y; y += 5) {
+			for (int x = color_rect.position.x; x < color_rect.position.x + color_rect.size.x - 5; x += 10) {
 				int xx = x;
 				int sx = 5;
-								
-				if(line_index % 2) {
+
+				if (line_index % 2) {
 					gc.SetColor(ax::Color(0.95));
 					gc.DrawRectangle(ax::Rect(xx, y, 5, 5));
 					gc.SetColor(ax::Color(0.99));
@@ -200,7 +163,7 @@ namespace inspector {
 					gc.DrawRectangle(ax::Rect(xx + 5, y, sx, 5));
 				}
 			}
-			
+
 			line_index++;
 		}
 
@@ -209,7 +172,7 @@ namespace inspector {
 
 		gc.SetColor(ax::Color(0.88));
 		gc.DrawRectangleContour(ax::Rect(rect.position, ax::Size(rect.size.x, rect.size.y + 1)));
-		
+
 		gc.SetColor(ax::Color(0.88));
 		gc.DrawLine(ax::Point(91, 0), ax::Point(91, rect.size.y + 1));
 	}
