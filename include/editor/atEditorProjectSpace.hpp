11bc16dc639f7f29a312468c48494668bb45c3b5
Alexandre Arsenault <aarsenault@erftcomposites.com>
2017-01-02 21:50:02 -0500

    Add icon color in project node tree menu.

--- include/editor/atEditorProjectSpace.hpp
+++ include/editor/atEditorProjectSpace.hpp
@@ -9,6 +9,9 @@ namespace editor {
 	public:
 		ProjectSpaceObj(const ax::Rect& rect, const std::string& name, ax::Window* widget, int level);
 
+		void UnSelectAll();
+		void SetSelectedWidgets(const std::vector<ax::Window*>& widgets);
+
 	private:
 		ax::Font _font;
 		ax::Font _font_bold;
@@ -16,6 +19,9 @@ namespace editor {
 		ax::Window* _widget;
 		int _level;
 		std::string _unique_name;
+		bool _is_selected = false;
+		std::shared_ptr<ax::Image> _icon;
+		ax::Color _icon_color;
 
 		void OnMouseLeftDoubleClick(const ax::Point& pos);
 		void OnPaint(ax::GC gc);
@@ -26,6 +32,12 @@ namespace editor {
 		ProjectSpace(const ax::Rect& rect);
 
 		void UpdateTree();
+		void UnSelectAll();
+		void SetSelectedWidgets(const std::vector<ax::Window*>& widgets);
+
+		axEVENT_DECLARATION(ax::event::SimpleMsg<ax::Window*>, OnSelectWidget);
+		axEVENT_DECLARATION(ax::event::SimpleMsg<int>, OnUnSelectAllWidget);
+		axEVENT_DECLARATION(ax::event::SimpleMsg<std::vector<ax::Window*>>, OnSelectMultipleWidget);
 
 	private:
 		ax::Font _font;
@@ -33,6 +45,7 @@ namespace editor {
 
 		ax::Window* _panel;
 		ax::ScrollBar* _scroll_bar;
+		bool _has_grid_window_connection = false;
 
 		void OnResize(const ax::Size& size);
 		void OnMouseLeftDown(const ax::Point& pos);
--- source/editor/atEditorProjectSpace.cpp
+++ source/editor/atEditorProjectSpace.cpp
@@ -38,11 +38,39 @@ namespace editor {
 			return;
 		}
 
+		// Icon.
+		if (_name == "Panel") {
+			_icon_color = ax::Color(0.2);
+			_icon = std::make_shared<ax::Image>("resources/tree_icon_panel.png");
+		}
+		else if (_name == "Button") {
+			_icon_color = ax::Color(200, 0, 0);
+			_icon = std::make_shared<ax::Image>("resources/tree_icon.png");
+		}
+		else if (_name == "Knob") {
+			_icon_color = ax::Color(200, 200, 0);
+			_icon = std::make_shared<ax::Image>("resources/tree_icon.png");
+		}
+		else if (_name == "Toggle") {
+			_icon_color = ax::Color(200, 0, 200);
+			_icon = std::make_shared<ax::Image>("resources/tree_icon.png");
+		}
+		else if (_name == "Sprite") {
+			_icon_color = ax::Color(0, 200, 200);
+			_icon = std::make_shared<ax::Image>("resources/tree_icon.png");
+		}
+		else {
+			_icon_color = ax::Color(0.7);
+			_icon = std::make_shared<ax::Image>("resources/tree_icon.png");
+		}
+
 		if (widget->component.Has("unique_name")) {
 			at::UniqueNameComponent::Ptr uname
 				= widget->component.Get<at::UniqueNameComponent>("unique_name");
 			_unique_name = uname->GetName();
 		}
+
+		// GetSelectedWindows
 
 		std::vector<std::shared_ptr<ax::Window>>& children = _widget->node.GetChildren();
 
@@ -64,6 +92,37 @@ namespace editor {
 		win->dimension.SetSize(ax::Size(rect.size.w, pos.y));
 	}
 
+	void ProjectSpaceObj::UnSelectAll()
+	{
+		if (_is_selected) {
+			_is_selected = false;
+			win->Update();
+		}
+
+		for (auto n : win->node.GetChildren()) {
+			n->GetBackbone<ProjectSpaceObj>()->UnSelectAll();
+		}
+	}
+
+	void ProjectSpaceObj::SetSelectedWidgets(const std::vector<ax::Window*>& widgets)
+	{
+		if (_widget == nullptr) {
+			return;
+		}
+
+		for (ax::Window* w : widgets) {
+			if (_widget->GetId() == w->GetId()) {
+				_is_selected = true;
+				win->Update();
+				break;
+			}
+		}
+
+		for (auto n : win->node.GetChildren()) {
+			n->GetBackbone<ProjectSpaceObj>()->SetSelectedWidgets(widgets);
+		}
+	}
+
 	void ProjectSpaceObj::OnMouseLeftDoubleClick(const ax::Point& pos)
 	{
 		editor::App* app = editor::App::GetInstance();
@@ -71,19 +130,27 @@ namespace editor {
 		editor::GridWindow* grid_win = main_win->GetGridWindow();
 		grid_win->GetWindow()->PushEvent(
 			at::editor::GridWindow::SELECT_WIDGET, new ax::event::SimpleMsg<ax::Window*>(_widget));
+
+		_is_selected = true;
+		win->Update();
 	}
 
 	void ProjectSpaceObj::OnPaint(ax::GC gc)
 	{
 		const ax::Rect rect(win->dimension.GetDrawingRect());
 
-		gc.SetColor(ax::Color(1.0));
+		_is_selected ? gc.SetColor(ax::Color(0.95)) : gc.SetColor(ax::Color(1.0));
 		gc.DrawRectangle(rect);
 
-		gc.SetColor(ax::Color(0.3));
+		if (_icon->IsImageReady()) {
+			gc.DrawImageColor(_icon.get(), ax::Point(10 + _level * 15, 6), _icon_color);
+		}
+
+		_is_selected ? gc.SetColor(ax::Color(250, 172, 0)) : gc.SetColor(ax::Color(0.3));
 		gc.DrawString(_font_bold, _name, ax::Point(25 + _level * 15, 4));
 
 		if (!_unique_name.empty()) {
+			gc.SetColor(ax::Color(0.3));
 			gc.DrawStringAlignedRight(
 				_font, _unique_name, ax::Rect(rect.position, rect.size - ax::Size(10, 0)));
 		}
@@ -92,6 +159,7 @@ namespace editor {
 		gc.DrawRectangleContour(rect);
 	}
 
+	//
 	ProjectSpace::ProjectSpace(const ax::Rect& rect)
 		: _font(0)
 		, _font_bold("fonts/FreeSansBold.ttf")
@@ -123,19 +191,25 @@ namespace editor {
 		win->node.Add(sb);
 
 		_panel->property.AddProperty("BlockDrawing");
-		_panel->dimension.SetSizeNoShowRect(ax::Size(rect.size.w, rect.size.h)); // pos.y));
+		_panel->dimension.SetSizeNoShowRect(ax::Size(rect.size.w, rect.size.h));
 
 		_scroll_bar->SetWindowHandle(_panel);
 		_scroll_bar->UpdateWindowSize(_panel->dimension.GetSize());
-
-		//		auto obj1 = std::make_shared<ProjectSpaceObj>(ax::Rect(0, 0, rect.size.w, 25), "Panel");
-		//		_panel->node.Add(obj1);
-
-		// CreateTree();
 	}
 
 	void ProjectSpace::UpdateTree()
 	{
+		if (_has_grid_window_connection == false) {
+			_has_grid_window_connection = true;
+			editor::App* app = editor::App::GetInstance();
+			editor::MainWindow* main_win = app->GetMainWindow();
+			editor::GridWindow* grid_win = main_win->GetGridWindow();
+			grid_win->GetWindow()->AddConnection(GridWindow::SELECT_WIDGET, GetOnSelectWidget());
+			grid_win->GetWindow()->AddConnection(GridWindow::UNSELECT_ALL, GetOnUnSelectAllWidget());
+			grid_win->GetWindow()->AddConnection(
+				GridWindow::SELECT_MULTIPLE_WIDGET, GetOnSelectMultipleWidget());
+		}
+
 		// Remove all children.
 		_panel->node.GetChildren().clear();
 		_panel->Update();
@@ -157,6 +231,43 @@ namespace editor {
 		_panel->dimension.SetSizeNoShowRect(
 			ax::Size(size.w, obj1->GetWindow()->dimension.GetRect().GetNextPosDown(0).y)); // pos.y));
 		_scroll_bar->UpdateWindowSize(_panel->dimension.GetSize());
+
+		SetSelectedWidgets(editor::App::GetInstance()->GetMainWindow()->GetSelectedWindows());
+	}
+
+	void ProjectSpace::UnSelectAll()
+	{
+		for (auto n : _panel->node.GetChildren()) {
+			n->GetBackbone<ProjectSpaceObj>()->UnSelectAll();
+		}
+	}
+
+	void ProjectSpace::SetSelectedWidgets(const std::vector<ax::Window*>& widgets)
+	{
+		for (auto n : _panel->node.GetChildren()) {
+			n->GetBackbone<ProjectSpaceObj>()->SetSelectedWidgets(widgets);
+		}
+	}
+
+	void ProjectSpace::OnSelectWidget(const ax::event::SimpleMsg<ax::Window*>& msg)
+	{
+		ax::Window* w = msg.GetMsg();
+		std::vector<ax::Window*> ws(1);
+		ws[0] = w;
+
+		UnSelectAll();
+		SetSelectedWidgets(ws);
+	}
+
+	void ProjectSpace::OnUnSelectAllWidget(const ax::event::SimpleMsg<int>& msg)
+	{
+		UnSelectAll();
+	}
+
+	void ProjectSpace::OnSelectMultipleWidget(const ax::event::SimpleMsg<std::vector<ax::Window*>>& msg)
+	{
+		UnSelectAll();
+		SetSelectedWidgets(msg.GetMsg());
 	}
 
 	void PrintWidgetName(ax::Window* w)
